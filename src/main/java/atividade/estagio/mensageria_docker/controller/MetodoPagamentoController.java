7f5581f072953e050af365bdb4d855fb544172b2
miguel-sr01 <miguel.sanches98b@gmail.com>
2025-10-14 16:24:11 -0300

    Configurando Versionamento API e Swagger via openApi

--- src/main/java/atividade/estagio/mensageria_docker/controller/MetodoPagamentoController.java
+++ src/main/java/atividade/estagio/mensageria_docker/controller/MetodoPagamentoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/metodos-pagamento")
+@RequestMapping("/api/v1/metodos-pagamento")
 public class MetodoPagamentoController {
 
 
